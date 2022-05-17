9502bba0de1d05a2e9524db465853d5a36e62546
Alex Paredes <alexparedesr95@gmail.com>
2022-05-17 16:24:35 -0500

    FEAT: Configured middleware to read and parse request body.

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ const app = express();
 app.use( express.static( 'public' ) );
 
 
+// Lectura y parseo del body.
+app.use( express.json() );
+
+
 // Rutas.
 app.use( '/api/auth', require( './routes/auth' ) );
 // TODO: CRUD: Eventos.
