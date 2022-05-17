c5c77456bc5314814c6c175de3365dcfa360f41d
Alex Paredes <alexparedesr95@gmail.com>
2022-05-17 16:11:33 -0500

    REFACTOR: Simplified routes by moving logic to controller and importing related function.

--- routes/auth.js
+++ routes/auth.js
@@ -4,13 +4,14 @@ host + /api/auth
  */
 
 const { Router } = require( 'express' );
+const { createUser, loginUser, renewToken } = require('../controllers/auth');
 const router = Router();
 
-router.get( '/', ( req, res ) => {
-    res.json({
-        ok: true
-    });
-});
+router.post( '/new', createUser );
+
+router.post( '/', loginUser );
+
+router.get( '/renew', renewToken );
 
 
 module.exports = router;
