0dcbab2018d5b0ec40c983ef1020eba2f4caffef
Alex Paredes <alexparedesr95@gmail.com>
2022-05-17 16:25:00 -0500

    FEAT: Obtained request body.

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,35 @@ const express = require("express");
 
 
 const createUser = ( req, res = express.response ) => {
+
+    const { name, email, password } = req.body;
+
+    if ( name.length < 2 ) {
+        return res.status( 400 ).json({
+            ok: false,
+            msg: 'Name should be at least 2 characters long.'
+        });
+    }
+    
     res.json({
         ok: true,
-        msg: 'registro'
+        msg: 'registro',
+        name,
+        email,
+        password
     });
 };
 
 
 const loginUser = ( req, res = express.response ) => {
+
+    const { email, password } = req.body;
+
     res.json({
         ok: true,
-        msg: 'login'
+        msg: 'login',
+        email,
+        password
     });
 };
 
