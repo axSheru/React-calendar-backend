b11839a6d0b1df41db07a4091b4efa42a8848b93
Alex Paredes <alexparedesr95@gmail.com>
2022-05-20 16:54:17 -0500

    FEAT: Implemented validarJWT middleware in renew endpoint.

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,7 @@ const { Router } = require( 'express' );
 const { check } = require('express-validator');
 const { createUser, loginUser, renewToken } = require('../controllers/auth');
 const { validarCampos } = require('../middlewares/validar-campos');
+const { validarJWT } = require('../middlewares/validar-jwt');
 const router = Router();
 
 router.post(
@@ -30,7 +31,7 @@ router.post(
     loginUser
 );
 
-router.get( '/renew', renewToken );
+router.get( '/renew', validarJWT, renewToken );
 
 
 module.exports = router;
