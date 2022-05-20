83d4405fd15245049ee5fa52509bcb0eeada6a3e
Alex Paredes <alexparedesr95@gmail.com>
2022-05-20 16:57:56 -0500

    CHORE: Configured CORS.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { dbConnection } = require('./database/config');
+const cors = require('cors');
 require( 'dotenv' ).config();
 
 // Crear el servidor de express.
@@ -8,6 +9,10 @@ const app = express();
 
 // Base de datos.
 dbConnection();
+
+
+// CORS
+app.use( cors() ); 
 
 
 // Directorio público.
