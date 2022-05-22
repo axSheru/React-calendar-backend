1acc2983e9ac85a61a3265881033f2ea41163374
Alex Paredes <alexparedesr95@gmail.com>
2022-05-22 00:26:51 -0500

    FEAT: Created routes for events CRUD.

---
+++ routes/events.js
@@ -0,0 +1,39 @@
+/* 
+Rutas de eventos / events
+host + /api/events
+ */
+
+const { Router } = require("express");
+const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require("../controllers/events");
+const { validarJWT } = require("../middlewares/validar-jwt");
+
+const router = Router();
+
+// Todas pasan por el middleware de validarJWT.
+router.use( validarJWT );
+
+// Obtener eventos.
+router.get(
+    '/',
+    getEventos
+);
+
+// Crear un nuevo evento.
+router.post(
+    '/',
+    crearEvento
+);
+
+// Actualizar evento.
+router.put(
+    '/:id',
+    actualizarEvento
+);
+
+// Eliminar evento.
+router.delete(
+    '/:id',
+    eliminarEvento
+);
+
+module.exports = router;
