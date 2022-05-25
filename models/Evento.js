ad50f885cdda99b53a41b268da3a93c6e2507973
Alex Paredes <alexparedesr95@gmail.com>
2022-05-24 21:17:37 -0500

    FIX: Changed require for required in models properties.

--- models/Evento.js
+++ models/Evento.js
@@ -4,22 +4,23 @@ const EventoSchema = Schema({
 
     title: {
         type: String,
-        require: true
+        required: true
     },
     notes: {
         type: String
     },
     start: {
         type: Date,
-        require: true
+        required: true
     },
     end: {
         type: Date,
-        require: true
+        required: true
     },
     user: {
         type: Schema.Types.ObjectId,
-        ref: 'Usuario'
+        ref: 'Usuario',
+        required: true
     }
 
 });
